eca9dd687c7b9330e0feebe6ca8f39421d788ff9
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-11-29 11:40:45 +0100

    Update ej1b3.py

--- 1b/ej1b3.py
+++ 1b/ej1b3.py
@@ -52,7 +52,11 @@ import matplotlib.pyplot as plt
 # Aquesta funció haurà de configurar la gràfica en la variable plt
 def line_graph(x, y):
     # Write here your code
-    pass
+    plt.plot(x, y)
+    plt.xlabel('Axis X')
+    plt.ylabel('Axis Y')
+    plt.title('Graph')
+    plt.grid(True)
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
